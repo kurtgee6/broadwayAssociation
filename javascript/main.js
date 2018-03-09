bab3c2feb9328e81e1b73fdde9b61c4c9c63cc6f
kurtgee6 <kurtgee11@gmail.com>
2018-03-09 13:05:43 -0500

    update interface

--- javascript/main.js
+++ javascript/main.js
@@ -15,9 +15,9 @@ $( document ).ready(function() {
 });
 
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: 40.7128, lng: -73.935242};
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 12,
       center: uluru
     });
     var marker = new google.maps.Marker({
